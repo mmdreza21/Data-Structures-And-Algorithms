9cd8f6527697e15956333d942a7dc78a43060d9a
mrezaj1377 <mrezaj1377@gmail.com>
2023-02-26 08:16:05 +0330

    linked list reverse is Done

--- Program.cs
+++ Program.cs
@@ -14,23 +14,24 @@ namespace DataStructures
 
 
             var list = new LinkedList();
-            //list.AddFirst(6);
-            //list.AddFirst(5);
-            //list.AddFirst(4);
-            //list.AddFirst(3);
-            //list.AddFirst(2);
-            //list.AddFirst(1);
+            list.AddFirst(6);
+            list.AddFirst(5);
+            list.AddFirst(4);
+            list.AddFirst(3);
+            list.AddFirst(2);
+            list.AddFirst(1);
+
             list.AddFirst(7);
             list.AddLast(8);
 
 
-            //list.RemoveFirst();
-            //list.RemoveLast();
+            list.RemoveFirst();
+            list.RemoveLast();
             list.Reverce();
 
-            //Console.WriteLine(list.Size());
+            Console.WriteLine(list.Size);
 
-            //foreach (var item in list.ConvertToArr()) { Console.WriteLine(item); }
+            foreach (var item in list.ConvertToArr()) { Console.WriteLine(item); }
 
 
         }
