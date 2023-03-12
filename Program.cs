647c6c552d8d16c4b56abda2482b11810036e7ec
mohamadreza <mrezaj11@live.com>
2023-03-12 17:52:30 +0330

    some simp nbog fixed

--- Program.cs
+++ Program.cs
@@ -13,32 +13,7 @@ namespace DataStructures
 
 
 
-            var list = new LinkedList();
 
-            //list.AddFirst(8);
-            list.AddFirst(7);
-            list.AddFirst(6);
-            list.AddFirst(5);
-            list.AddFirst(4);
-            list.AddFirst(3);
-            list.AddFirst(2);
-            list.AddFirst(1);
-
-            //list.AddFirst(7);
-            //list.AddLast(8);
-            //list.AddLast(2);
-            //list.AddLast(1);
-
-
-            //list.RemoveFirst();
-            //list.RemoveLast();
-            //list.Reverce();
-            //list.GetKthFromTheEnd(2);
-            Console.WriteLine(list.printMiddle());
-       
-
-
-            //Console.WriteLine(list.GetKthFromTheEnd(100));
 
 
 
