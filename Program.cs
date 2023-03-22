8d138d43405f31c09c9ea39a53518c5ac691c578
mrezaj1377 <mrezaj1377@gmail.com>
2023-03-22 15:21:23 +0330

    stack with array done

--- Program.cs
+++ Program.cs
@@ -10,11 +10,13 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Pop();
 
-
-
-
-
+            Console.WriteLine(stack.ToString());
         }
     }
 }
--- Stack.cs
+++ Stack.cs
@@ -1,26 +1,62 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace DataStructures
 {
     public class Stack
     {
 
-        private int[] _satck;
+        private int[] _items = new int[10];
 
         //count
-        private int count = 0;
-
-
+        public int count { get; private set; }
 
         //push
-        public void push(int val)
+        public void Push(int val)
         {
+            if (count == _items.Length)
+                throw new StackOverflowException("stack overflow...");
 
+            _items[count++] = val;
         }
 
         //pop
+        public int Pop()
+        {
+            if (IsEmpty())
+                throw new ArgumentOutOfRangeException("the stack is allReady empty");
+            var last = _items[count];
+            _items[count--] = 0;
+            return last;
+        }
         //peek
+        public int Peek()
+        {
+            if (IsEmpty())
+                throw new ArgumentOutOfRangeException("the stack is allReady empty");
+            return _items[count];
+        }
         //isEmpty
+        public bool IsEmpty()
+        {
+            return count == 0;
+        }
+
+
+        public override string ToString()
+        {
+            var str = "[";
+            for (int i = 0; i < count; i++)
+            {
+                str += _items[i].ToString();
+
+                if (i < count - 1)
+                    str += " , ";
+            }
+            str += "]";
+            return str;
+
+        }
 
 
 
